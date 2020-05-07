5efb1117ba8408e97f39e9c8f085a486b3238b3a
unknown <rzc307853639@gmail.com>
2020-05-07 16:57:18 +0800

    ran

--- src/routes/user/login.ts
+++ src/routes/user/login.ts
@@ -11,11 +11,15 @@ router.post('/login', (req, res) => {
     userName,
     password: MD5(password + constant.SECRET_USER_STRING).toString(),
   };
-  findByNameUser(body, (err: any, data: Document | null) => {
+  findByNameUser(body, (err: any, data: any) => {
     if (err) {
       return res.status(500).send({ status: 0, msg: '服务器错误请稍后再试' });
     }
     if (data) {
+      if (req.session) {
+        //设置session,在权限鉴定的时候通过这个判断是否登录,在get的同时
+        req.session.user = data.userName;
+      }
       return res.send(data);
     }
     return res.send({ status: 0, msg: '账号或者密码错误' });
