75aedcadcf3d19fa7a3262f6c696bf6f03915f4f
unknown <rzc307853639@gmail.com>
2020-05-08 18:28:22 +0800

    https

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
-import http from 'http';
+import https from 'https';
 import socket from 'socket.io';
 import chat from './chat';
 import setConfig from './config';
+import path from 'path';
+import fs from 'fs';
 
 const app = express();
-const server = http.createServer(app);
+const cert = fs.readFileSync(path.join(__dirname, '../cert/cert.pem'));
+const key = fs.readFileSync(path.join(__dirname, '../cert/key.pem'));
+const options = { key, cert };
+
+const server = https.createServer(options, app);
 const io = socket(server);
 const port = 5050;
 
@@ -16,5 +22,5 @@ chat(io);
 setConfig(app);
 
 server.listen(port, () =>
-  console.log(`server is running at http://127.0.0.1:${port}`)
+  console.log(`server is running at https://127.0.0.1:${port}`)
 );
