4f135ca3f78debb1f77afbe7fef8ff313588682f
unknown <rzc307853639@gmail.com>
2020-05-14 12:19:28 +0800

    clear services

--- src/services/error/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.all('*', (req, res) => {
-  res.status(404).send({ status: 0, data: { msg: 'error' } });
-});
-
-export default router;
