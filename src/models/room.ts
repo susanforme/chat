bdb737d85a9a85396d1580631fb9b563a7447b2d
unknown <rzc307853639@gmail.com>
2020-06-10 17:47:17 +0800

    数据库

--- src/models/room.ts
+++ src/models/room.ts
@@ -1,10 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/chat', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
 const Schema = mongoose.Schema;
 const RoomSchema = new Schema({
   roomId: {
